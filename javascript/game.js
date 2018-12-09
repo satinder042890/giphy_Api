6b980e3575189188835f27e4199b6e8ac72441db
satinder <satinder042890@gmail.com>
2018-12-08 17:52:45 -0700

    submit button is working

--- javascript/game.js
+++ javascript/game.js
@@ -1,13 +1,11 @@
-var topics=["Eating","Crying","Smiling","Angry","Sad","Hyper","Cooking"];
+var topics=["Eating","Crying","Smiling","Angry","Sad","Hyper","Cooking","Driving","Sleeping","Chating","Travelling","Thinking","Reading"];
 var s="";
 var queryUrl;
 function renderButton(){
+    console.log(topics);
     $("#buttonDisplayArea").empty();
     for(var i=0; i<topics.length ; i++){
-        var bt=$("<button>");
-        bt.addClass("buttonForActions");
-        bt.attr("data-name",topics[i]);
-        bt.text(topics[i]);
+        var bt=$("<button>").addClass("buttonForActions").attr("data-name",topics[i]).text(topics[i]);
         $("#buttonDisplayArea").append(bt);
     }
 }
@@ -22,19 +20,41 @@ $.ajax({
     method:"GET"
 }).then(function(response){
     console.log(response);
-    console.log(queryUrl);
     for(var i=0; i<response.data.length;i++){
-        var image=$("<img>");
-        image.attr("src",response.data[i].images.downsized.url);
-        image.addClass("imageSize");
-        image.attr(response.data[i]);
-        $("#gifDisplayArea").append(image);
-    //    $("#gifDisplayArea").append("<img src ='"+response.data[i].images.downsized.url+"'>");
+        var box=$("<div>").addClass("mainBox");
+        var image=$("<img>").attr("src",response.data[i].images.downsized_still.url).addClass("imageSize").attr("data-name-still",response.data[i].images.downsized_still.url).attr("data-name-moving",response.data[i].images.downsized.url).attr("image-state","still");
+        console.log(i+response.data[i].rating);
+        var rating=$("<b>").text("Rating : "+response.data[i].rating).addClass("imageBox");
+        box.append(rating);
+        box.append(image);
+        $("#gifDisplayArea").prepend(box);
     }
+    $(".imageSize").on("click",function(){
+        var state=$(this).attr("image-state");
+        if(state === "still"){
+            $(this).attr("src",$(this).attr("data-name-moving"));
+            state= $(this).attr("image-state","moving");
+        }
+        else{
+            $(this).attr("src",$(this).attr("data-name-still"));
+            state= $(this).attr("image-state","still");
+        }
+    });
 });
 }
+function addNewValues(){
+    var textValue=$("#textBox").val();
+    console.log(textValue);
+    topics.push(textValue);
+    $("#textBox").val("");
+    renderButton();
+
+}
+
 $(document).on("click", ".buttonForActions", displayGif);
+$(document).on("click", "#submitButton", addNewValues);
 
 $(document).ready(function(){
     renderButton();
+   
 });
