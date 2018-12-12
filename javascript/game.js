63b2f428bd892f9e497dab099e9883bc2fe8faf9
satinder <satinder042890@gmail.com>
2018-12-12 16:26:24 -0700

    gif added bug fixed

--- javascript/game.js
+++ javascript/game.js
@@ -2,6 +2,7 @@ var topics=["Eating","Crying","Smiling","Angry","Sad","Hyper","Cooking","Driving
 var s="";
 var queryUrl;
 var l=10;
+//function to display buttons on screen
 function renderButton(){
     console.log(topics);
     $("#buttonDisplayArea").empty();
@@ -10,38 +11,42 @@ function renderButton(){
         $("#buttonDisplayArea").append(bt);
     }
 }
+//function 
 function displayGif(){
     $("#gifDisplayArea").empty();  
     console.log(this);
-s=$(this).attr("data-name");
-queryUrl="https://api.giphy.com/v1/gifs/search?q=" + s + "&api_key=003zWj7bkLEFl3HLOs0VQCIZzsx6RKHL&limit=" +l;
-console.log(s);
-$.ajax({
-    url:queryUrl,
-    method:"GET"
-}).then(function(response){
-    console.log(response);
-    for(var i=0; i<response.data.length;i++){
-        var box=$("<div>").addClass("mainBox");
-        var image=$("<img>").attr("src",response.data[i].images.downsized_still.url).addClass("imageSize").attr("data-name-still",response.data[i].images.downsized_still.url).attr("data-name-moving",response.data[i].images.downsized.url).attr("image-state","still");
-        console.log(i+response.data[i].rating);
-        var rating=$("<b>").text("Rating : "+response.data[i].rating).addClass("imageBox");
-        box.append(rating);
-        box.append(image);
-        $("#gifDisplayArea").prepend(box);
+    s=$(this).attr("data-name");
+    queryUrl="https://api.giphy.com/v1/gifs/search?q=" + s + "&api_key=003zWj7bkLEFl3HLOs0VQCIZzsx6RKHL&limit=" +l;
+    console.log(s);
+    $.ajax({
+       url:queryUrl,
+       method:"GET"
+    }).then(function(response){
+        console.log(response);
+        apiCall(response)
+    });
+}
+function apiCall(response){
+   for(var i=0; i<response.data.length;i++){
+       var box=$("<div>").addClass("mainBox");
+       var image=$("<img>").attr("src",response.data[i].images.downsized_still.url).addClass("imageSize").attr("data-name-still",response.data[i].images.downsized_still.url).attr("data-name-moving",response.data[i].images.downsized.url).attr("image-state","still");
+       console.log(i+response.data[i].rating);
+       var rating=$("<b>").text("Rating : "+response.data[i].rating).addClass("imageBox");
+       box.append(rating);
+       box.append(image);
+       $("#gifDisplayArea").prepend(box);
     }
     $(".imageSize").on("click",function(){
         var state=$(this).attr("image-state");
         if(state === "still"){
-            $(this).attr("src",$(this).attr("data-name-moving"));
-            state= $(this).attr("image-state","moving");
+           $(this).attr("src",$(this).attr("data-name-moving"));
+           state= $(this).attr("image-state","moving");
         }
         else{
-            $(this).attr("src",$(this).attr("data-name-still"));
-            state= $(this).attr("image-state","still");
+           $(this).attr("src",$(this).attr("data-name-still"));
+           state= $(this).attr("image-state","still");
         }
     });
-});
 }
 function addNewValues(){
     event.preventDefault();
@@ -58,13 +63,13 @@ function addNumberForGifs(){
     l+=parseInt($("#textBoxForNumber").val());
     console.log(l);
     $("#textBoxForNumber").val("");
-    displayGif();
+    // displayGif();
 }
 function resetGifValues(){
     event.preventDefault();
     l=10;
     console.log(l);
-    displayGif();
+    // displayGif();
 }
 
 $(document).on("click", ".buttonForActions", displayGif);
